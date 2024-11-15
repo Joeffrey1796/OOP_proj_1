6e8384cb7f6ba9704d38cd7721232ce8c085dc26
Christian Joeffrey Castro <castrochristianjoeffrey10@gmail.com>
2024-11-15 15:48:39 +0800

    Made the program into a class

--- oop_project.py
+++ oop_project.py
@@ -4,169 +4,153 @@ OOP database system proj
 
 import typing as tp
 
+class Inventory_Management_System:
 
-# inventory: tp.Dict[tp.Any,tp.Any] = dict()
-# transactions: tp.List[tp.Any] = list()
+    def __init__(self):
+        '''
+        Class Constructor
+        '''
+        self.categories: tp.Set[tp.Any] = set()
+        self.structure: tp.Dict[str,tp.List[tp.Dict[str,tp.List[int|float]]]] = dict()
+        
+    def main_loop(self) -> None:
+        '''
+        Main Function of the program
+        '''
 
-categories: tp.Set[tp.Any] = set()
-structure: tp.Dict[str,tp.List[tp.Dict[str,tp.List[int|float]]]] = dict()
-
-def main() -> None:
-    '''Main function'''
-
-    while True:
-        print("""
+        while True:
+            print("""
 Inventory Management System
     1. Add Item
     2. Edit Item Quantity
     3. Delete Item
     4. Display Inventory by Category
     5. Summary Report
-    6. Exit          
-            """)
- 
-        choice = input("    Choose an option (1-6): ")
+    6. Exit
+""")
+            choice : str = input("\t Choose an option (1-6): ")
 
-        if choice == '1':
-            add_item()
-        elif choice == '2':
-            edit()
-        elif choice == '3':
-            delete()            
-        elif choice == '4':
-            display()
-        elif choice == '5':
-            summary()
-        elif choice == '6':
-            break
-        else:
-            print("Invalid option. Please choose a valid option")
-
-
-
-
-def is_int(num) -> bool:
-    try:
-        int(num)
-        return True
-    except Exception:
-        return False
-
-def is_float(num) -> bool:
-    try:
-        float(num)
-        return True
-    except Exception:
-        return False
-
-def add_item() -> None:
-    item_name : str = input("Enter item name: ")
-    category : str = input("Enter category: ")
-
-    if category in categories:
-        for i in structure[category]:
-            if item_name in i:
-                print("No duplicate items(update instead of add?)")
-                return
-
-
-    quantity : str|int = input("Enter quantity: ")
-    if is_int(quantity):
-        quantity = int(quantity)
-    else:
-        print("Invalid quantity. Please enter a number")
-        return
-
-    price : str|float = input("Enter price: ")
-    if is_float(price):
-        price = float(price)
-    else:
-        print("Invalid quantity. Please enter a number")
-        return
-
-
-    
-
-    # print(structure)
-    # print(categories)
-
-    if category not in categories:
-        categories.add(category)
-        structure[category] = [{item_name: [quantity, price]}]
-    else:
-        structure[category].append({item_name:[quantity,price]})
-    
-
-def edit() -> None:
-    category : str = input("Enter the category of the item to update: ")
-    if category not in categories:
-        print("Category not found in the system")
-        return
-    
-    item : str = input("Enter the name of the item to update: ")
-    if not any((item in s_v_dict) for s_v_dict in structure[category]):
-        print("Item not found in the system")
-        return
-    
-
-    update : str|int = input("Enter the quantity change(positive to add, negative to remove): ")
-    if is_int(update):
-        update = int(update)
-        for s_v_dict in structure[category]:
-            if item in s_v_dict:
-                s_v_dict[item][0] += update
-                print(f"Item '{item}' quantity updated to {s_v_dict[item][0]}")
+            if choice == '1':
+                self.add_item()
+            elif choice == '2':
+                self.edit()
+            elif choice == '3':
+                self.delete()
+            elif choice == '4':
+                self.display()
+            elif choice == '5':
+                self.summary()
+            elif choice == '6':
+                break
             else:
-                print("Item is not in the Inventory")
-    else:
-        print("Invalid quantity. Please enter a number")
-        return
-    
-def delete() -> None :
-    category : str = input("Enter the category of the item to delete: ")
-    if category not in categories:
-        print("Category not found in the system")
-        return
-    item : str = input("Enter the name of the item to delete: ")
+                print("Invalid option. Please choose a valid option.")
 
-    
-    if not any((item in s_v_dict) for s_v_dict in structure[category]):
-        print("Item not found in the system")
-        return
+    def is_int(self, num: str) -> bool:
+        try:
+            int(num)
+            return True
+        except ValueError:
+            return False
         
-    
-    for s_v_dict in structure[category]:
-        if item in s_v_dict:
-            s_v_dict.pop(item)
-            print(f"{item} removed from inventory")
+    def is_float(self, num: str) -> bool:
+        try:
+            float(num)
+            return True
+        except ValueError:
+            return False
+        
+    def add_item(self) -> None:
+        item_name: str = input("Enter item name: ")
+        category: str = input("Enter category: ")
+
+        if category in self.categories:
+            for s_dict in self.structure[category]:
+                if item_name in s_dict:
+                    print("No duplicate items(update instead of add?)")
+                    return None
+
+        quantity: int|str = input("Enter quantity: ")
+        if self.is_int(quantity):
+            quantity = int(quantity)
         else:
-            print("Item is not in the Inventory")
+            print("Invalid quantity. Please enter a number")
+            return None
+
+        price: float|str = input("Enter price: ")
+        if self.is_float(price):
+            float(price)
+        else:
+            print("Invalid quantity. Please enter a number")
+            return None
 
 
-def display() -> None:
-    for s_key, s_value in structure.items():
-        print(f"Category: {s_key}")
-        for s_v_list in s_value:
-            for s_v_key, s_v_value in s_v_list.items():
-                print(f"    {s_v_key}:\t Quantity = {s_v_value[0]},\t Price = {s_v_value[1]}")
-        print()
-    
+        if category not in self.categories:
+            self.categories.add(category)
+            self.structure[category] = [{item_name: [quantity, price]}]
+        else:
+            self.structure[category].append({item_name:[quantity,price]})
 
-def summary() -> None:
-    print("Inventory summary report:")
-    print(f"Total unique categories: {len(categories)}")
+    def edit(self) -> None:
+        category: str = input("Enter the category of the item to update: ")
+        if category not in self.categories:
+            print("Category not found in the system")
+            return None
+        
+        item: str = input("Enter the name of the item to update: ")
+        if not any((item in s_v_dict) for s_v_dict in self.structure[category]):
+            print("Item not found in the system")
+            return None
+        
+        update: str|int = input("Enter the quantity change (positive to add, negative to remove): ")
+        if self.is_int(update):
+            update = int(update)
+            for s_v_dict in self.structure[category]:
+                if item in s_v_dict:
+                    s_v_dict[item][0] += update
+                    print(f"Item '{item}' quantity updated to {s_v_dict[item][0]}")
+                else:
+                    print("Item is not in the inventory")
+        else:
+            print("Invalid quantity. Please enter a number")
+            return None
+        
+    def delete(self) -> None:
+        category: str = input("Enter the category of the item to delete: ")
+        if category not in self.categories:
+            print("Category not found in the system")
+            return None
+        item: str = input("Enter the name of the item to delete: ")
 
-    for key in structure:
-        print(f"Category: '{key}': {len(structure[key])} unique items")
-    
+        if not any((item in s_v_dict) for s_v_dict in self.structure[category]):
+            print("Item not foudn in the system")
+            return None
+        
+        for s_v_dict in self.structure[category]:
+            if item in s_v_dict:
+                s_v_dict.pop(item)
+                print(f"{item} removed from the inventory")
+            else:
+                print("Item is not in the inventory")
+
+    def display(self) -> None:
+        for s_key, s_value in self.structure.items():
+            print(f"Categofy: {s_key}")
+            for s_v_list in s_value:
+                for s_v_key, s_v_value in s_v_list.items():
+                    print(f"\t{s_v_key}:\tQuatity = {s_v_value[0]},\tPrice = {s_v_value[1]}")
+            print()
+
+    def summary(self) -> None:
+        print("Inventory summary report:")
+        print(f"Total unique categories: {len(self.categories)}")
+
+        for key in self.structure:
+            print(f"Category: '{key};: {len(self.structure[key])} unique items")
+
 
 if __name__ == "__main__":
-
-    # category = {"belt"}
-    # inventory = {"my belt":[12,12.0]}
-    # add_item()
-    # add_item()
-    main()
+    x = Inventory_Management_System()
+    x.main_loop()
 
     
 
